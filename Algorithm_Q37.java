d6c62324118ed6082ddc52dfea4245992bae30b6
JangGeunSong <sjg1468@gmail.com>
2022-11-14 21:40:03 +0900

    37일차 알고리즘 학습내용 나의 풀이 틀린 코드 추가

--- Algorithm_Q37.java
+++ Algorithm_Q37.java
@@ -7,6 +7,7 @@ public class Algorithm_Q37 {
      * 풀이를 확인해보자.
      */  
 
+    // 정답 코드
     public int minDistance(String word1, String word2) {
         // Edit distance 를 구하기 위해서는 표를 그려놓고 생각해 봐야 한다.
         // 이 연산에서 중요한 것은 h 와 "" 가 존재할때 h혹은 ""를 만들기 위한 연산 횟수를
@@ -41,6 +42,62 @@ public class Algorithm_Q37 {
         }
 
         answer = costs[m][n];
+
+        return answer;
+    }
+
+    // 틀린 나의 풀이.
+    // 순회를 통해 해결하려 했으나, 그 과정이 쉽지 않았다.
+    // 위의 원리를 이해하지 못해 진행할 수 없었던 부분이었다.
+    public int minDistance_mine_wrong_answer(String word1, String word2) {
+        // remove, replace, insert를 하는 조건은 무엇인가 확인해야 함.
+        // remove, replace, insert는 모두 일치하지 않는 단어의 개수를 세고 그 단어 수 만큼
+        // 연산 해야 하는 숫자이다.
+        // 이는 왼쪽, 오른쪽 모두 순회해서 최대값을 구해야 하고, 그 이후 최대 문장의 길이 - 일치 단어수
+        // 를 계산 하면 정답을 얻을 수 있다.
+        int answer = 0;
+        int idx = 0;
+        int maxSize = 0;
+        int matchedNumLeft = 0;
+        int matchedNumRight = 0;
+        char[] arr_word1 = word1.toCharArray();
+        char[] arr_word2 = word2.toCharArray();
+        
+        if(arr_word1.length == 0) {
+            return arr_word2.length;
+        } else if(arr_word2.length == 0) {
+            return arr_word1.length;
+        }
+
+        for(int i = 0; i < word1.length(); i++) {
+            if(arr_word1[i] == arr_word2[idx]) {
+                idx += 1;
+                matchedNumLeft += 1;
+            }
+            if(idx == arr_word2.length) {
+                break;
+            }
+        }
+
+        idx = arr_word2.length - 1;
+
+        for(int i = word1.length() - 1; i >= 0; i--) {
+            if(arr_word1[i] == arr_word2[idx]) {
+                matchedNumRight += 1;
+                idx -= 1;
+            }
+            if(idx == -1) {
+                break;
+            }
+        }
+
+        maxSize = Math.max(word1.length(), word2.length());
+
+        if(matchedNumLeft > matchedNumRight) {
+            answer = maxSize - matchedNumLeft;
+        } else {
+            answer = maxSize - matchedNumRight;
+        }
 
         return answer;
     }
