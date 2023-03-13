e8ddc52af55fa46a7d5102b2dda37dec90dbf6ab
JangGeunSong <sjg1468@gmail.com>
2023-03-13 22:23:07 +0900

    125일차 알고리즘 학습내용 - Q45 문제 BFS 모델로 다시 풀기 완료

--- Algorithm_Q45.java
+++ Algorithm_Q45.java
@@ -38,6 +38,75 @@ public class Algorithm_Q45 {
         }
     }
 
+    // 2023년 3월 13일 BFS 모델을 사용한 풀이
+    public boolean isSymmetric_bfs_model(TreeNode root) {
+        // 이번에는 BFS 모델로 풀이했다.
+        // 종료조건 root의 left와 right가 모두 null 일때 -> 노드가 1개 이므로, true로 리턴
+        if(root.left == null && root.right == null) {
+            return true;
+        }
+
+        // BFS 모델이므로, queue를 만들어 인접 노드들을 넣을 수 있게 선언
+        Queue<TreeNode> treeNodeQueue = new LinkedList<>();
+        // queue가 인접 노드를 접근 할 때, List에 숫자를 삽입하도록 처리
+        List<Integer> elemList = new ArrayList<>();
+        // depth를 구분하기 위해 선언
+        int countChildNodes = 1;
+        // 현재 depth에서 자식 노드들이 대칭으로 나타나는지 체크하기 위한 left & right 선언
+        int left = 0;
+        int right = 0;
+
+        // root를 queue에 삽입
+        treeNodeQueue.offer(root);
+
+        // queue가 비어 있지 않다면 반복문을 계속 처리
+        while(!treeNodeQueue.isEmpty()) {
+            // queue 에서 노드 하나를 poll
+            TreeNode node = treeNodeQueue.poll();
+
+            // 각 자식 노드별로 비어 있지 않다면, 노드를 큐에 넣고 해당 노드의 값을 리스트에 삽입
+            if(node.left != null) {
+                treeNodeQueue.offer(node.left);
+                elemList.add(node.left.val);
+            } else {
+                // 만약 자식 노드가 널이면 범위에 없는 숫자인 -101을 삽입
+                elemList.add(-101);
+            }
+
+            // left 자식과 마찬 가지로 처리
+            if(node.right != null) {
+                treeNodeQueue.offer(node.right);
+                elemList.add(node.right.val);
+            } else {
+                elemList.add(-101);
+            }
+            
+            // depth == 현재 queue의 사이즈 이므로, 사이즈 1을 줄여서 depth가 끝나는 것을 표시한다.
+            countChildNodes -= 1;
+            // countChildNodes 가 0 이면 현재 depth가 종료 된 것이므로, 다음 depth를 위해 큐의 size를 받아 depth
+            // 표시자로 처리하고 만들어진 list를 순회하여 대칭인지 체크한다.
+            if(countChildNodes == 0) {
+                countChildNodes = treeNodeQueue.size();
+                left = 0;
+                right = elemList.size() - 1;
+                // 대칭은 가장 처음 값과 가장 마지막 값이 같으며, 각각 1칸씩 가까워 질때에도 계속 값이 같아야 한다.
+                // 만약 이 과정 중 하나라도 값이 다르다면, 바로 false를 리턴하여 함수 종료
+                while(left < right) {
+                    if(elemList.get(left) != elemList.get(right)) {
+                        return false;
+                    }
+                    left += 1;
+                    right -= 1;
+                }
+                // 이번 depth에서 확인한 list를 clear 해서 값을 모두 삭제한다.
+                elemList.clear();
+            }
+        }
+
+        // 위 반복문을 모두 돌아도 false가 나오지 않는다면 대칭이므로, true를 리턴
+        return true;
+    }
+
     // ArrayList를 사용한 풀이 => 재귀 풀이
     public boolean isSymmetric_recursion(TreeNode root) {
         List<Integer> left = new ArrayList<>();
